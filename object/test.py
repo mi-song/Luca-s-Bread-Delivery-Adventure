8f91756c1b013dc8b1ffb9db9c8c2ba2372adc2d
mi-song <misong023@khu.ac.kr>
2023-11-04 04:42:10 +0900

    [FIX] place origin place/image after drop-click

--- object/test.py
+++ object/test.py
@@ -27,32 +27,35 @@ class Draggable:
         self.drop_time = 0
 
     def handle_event(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN and not self.dropped:
+        if event.type == pygame.MOUSEBUTTONDOWN:
             if self.rect.collidepoint(event.pos):
-                self.dragging = True
-                mouse_x, mouse_y = event.pos
-                self.offset_x = self.rect.x - mouse_x
-                self.offset_y = self.rect.y - mouse_y
+                # 드래그 상태가 아니고, 드롭된 상태에서 클릭했을 때, 원래 이미지와 위치로 복귀
+                if not self.dragging and self.dropped:
+                    self.change_image(self.original_image)
+                    self.rect.topleft = self.original_pos
+                    self.dropped = False
+                # 드래그 시작
+                else:
+                    self.dragging = True
+                    mouse_x, mouse_y = event.pos
+                    self.offset_x = self.rect.x - mouse_x
+                    self.offset_y = self.rect.y - mouse_y
 
         elif event.type == pygame.MOUSEBUTTONUP and self.dragging:
+            # 드래그 종료
             self.dragging = False
             self.dropped = True
-            self.drop_time = pygame.time.get_ticks()  # 드롭 시간 기록
-            self.change_image(self.drop_image)  # 드롭 이미지로 변경
+            self.drop_time = pygame.time.get_ticks()
+            self.change_image(self.drop_image)
 
         elif event.type == pygame.MOUSEMOTION and self.dragging:
+            # 드래그 중 이미지 이동
             mouse_x, mouse_y = event.pos
             self.rect.x = mouse_x + self.offset_x
             self.rect.y = mouse_y + self.offset_y
 
     def change_image(self, new_image):
         self.image = new_image
-
-    def reset_after_delay(self):
-        if self.dropped and pygame.time.get_ticks() - self.drop_time > 3000:  # 3초가 지났는지 확인
-            self.change_image(self.original_image)  # 원래 이미지로 변경
-            self.rect.topleft = self.original_pos  # 원래 위치로 이동
-            self.dropped = False  # 드래그 상태 초기화
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
@@ -69,7 +72,7 @@ while running:
             running = False
         draggable.handle_event(event)
 
-    draggable.reset_after_delay()  # 상태를 확인하고 필요한 경우 리셋
+    # draggable.reset_after_delay()  # 상태를 확인하고 필요한 경우 리셋
 
     # 화면 색상 설정 (RGB)
     screen.fill((255, 255, 255))
